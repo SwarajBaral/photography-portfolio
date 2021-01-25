afd54cbdb20e5c9db588c5aa90cfc426d279ade6
SwarajBaral <baralswaraj40@gmail.com>
2021-01-26 00:48:27 +0530

    Added the js file

---
+++ Deba/site.js
@@ -0,0 +1,9 @@
+var parallax = document.querySelectorAll('.parallax');
+var prevScrollpos = window.pageYOffset;
+
+window.addEventListener("scroll", function () {
+	let offset = window.pageYOffset;
+	for (var i = 0; i < parallax.length; i++) {
+		parallax[i].style.backgroundPositionY = offset * 0.6 + "px";
+	}console.log(offset);
+})
